6a7c51843a84967df270d8ebbec0d9834b419779
Abdallah-Alrawi <abdallah.alrawi43@gmail.com>
2022-09-13 23:18:44 +0300

    First

--- Store/src/Comps/NavBar.tsx
+++ Store/src/Comps/NavBar.tsx
@@ -5,16 +5,16 @@ import { useShoppingCart } from "../cont/shoppingCart"
 export function NavBar(){
     const {openCart, cartQuantity}= useShoppingCart()
     return (
-    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-dark shadow-sm mb-3 ">
         <Container>
             <Nav className="me-auto">
-                <Nav.Link to={"/"} as={NavLink}>
+                <Nav.Link to={"/"} as={NavLink} className="text-light">
                 Home
                 </Nav.Link>
-                <Nav.Link to={"/store"} as={NavLink}>
+                <Nav.Link to={"/store"} as={NavLink} className="text-light">
                 Store
                 </Nav.Link>
-                <Nav.Link to={"/about"} as={NavLink}>
+                <Nav.Link to={"/about"} as={NavLink} className="text-light">
                 About
                 </Nav.Link>
 
@@ -27,7 +27,7 @@ export function NavBar(){
             className="rounded-circle"
             > 
 
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-cart3" viewBox="0 0 16 16">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg>
             <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
--- Store/src/Pages/Home.tsx
+++ Store/src/Pages/Home.tsx
@@ -1,5 +1,24 @@
+
 export function Home(){
+   
     return (
-    <h1>Home</h1>
+       
+        <>
+       
+        <div className="d-flex align-items-center justify-content-center text-light">
+        <h1>Welcome to The store where you will meet all your tech needs</h1>
+        </div>
+        <div className="d-flex align-items-center justify-content-center mt-5 text-warning">
+            <h2>Click The button below to start your journey</h2>
+            </div>
+            <div className="d-flex align-items-center justify-content-center mb-2">
+            <img src='https://i.pinimg.com/736x/a1/e5/cb/a1e5cb750fbad2d269923b0c6b0bd918.jpg' className='img-fluid shadow-4'/>
+            </div>
+            <div className="d-flex align-items-center justify-content-center mb-5">
+            <a href="/store" className="btn btn-success" role="button">The Store</a>
+        </div>
+        
+        </>
+       
     )
 }
