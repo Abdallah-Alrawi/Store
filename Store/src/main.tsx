0aafb9a98d55873533ef8faadcf1a2c6f8d60427
Abdallah-Alrawi <abdallah.alrawi43@gmail.com>
2022-09-18 16:33:53 +0300

    Home page

--- Store/src/main.tsx
+++ Store/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter} from "react-router-dom"
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
